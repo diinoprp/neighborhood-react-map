299f32a6763efeed49cc8cffec22c574783546f8
Eduardo Santos <eduardosantos.ist@gmail.com>
2019-03-17 16:40:05 -0300

    Filtrando Lista

--- src/component/AppMenu.js
+++ src/component/AppMenu.js
@@ -9,19 +9,57 @@ class AppMenu extends Component {
     super(props);
 
     this.state = {
-      menuOpen: false
+      menuOpen: false,
+      query: '',
+      venues: []
     }
   }
 
   handleMenuClick() {
-    this.setState({menuOpen: !this.state.menuOpen})
+    this.setState({ menuOpen: !this.state.menuOpen })
   }
+
+  handleFilterVenues = () => {
+    if (this.state.query.trim() !== "") {
+      const venues = this.props.venues.filter(venue =>
+        venue.name
+          .toLowerCase()
+          .includes(this.state.query.toLowerCase()))
+      return venues;
+    }
+    return this.props.venues;
+  };
+
+  handleChange = e => {
+    this.setState({ query: e.target.value });
+
+    const markers = this.props.venues.map(venue => {
+      const isMatched = venue.name.toLowerCase().includes(e.target.value.toLowerCase());
+      const marker = this.props.markers.find(marker => marker.id === venue.id);
+      if (isMatched) {
+        marker.isVisible = true;
+      } else {
+        marker.isVisible = false;
+      }
+
+      return marker;
+    });
+
+    this.props.updateSuperState({ markers })
+  };
 
   render() {
     return (
       <div>
         <Menu className="venueList" isOpen={this.state.menuOpen}>
-          <VenueList {...this.props} handleListItemClick={this.props.handleListItemClick}/>
+          <input
+            type={"search"}
+            className={"searchInput"}
+            placeholder={"Filter Venues"}
+            onChange={this.handleChange}
+            aria-label="Filter Venues"
+          />
+          <VenueList {...this.props} filterVenues={this.handleFilterVenues()} handleListItemClick={this.props.handleListItemClick} />
         </Menu>
         <Navbar bg="dark" style={{ zIndex: 1 }}>
           <HamburgerButton
--- src/component/VenueList.js
+++ src/component/VenueList.js
@@ -1,50 +1,16 @@
 import React, { Component } from 'react';
-import escapeRegExp from 'escape-string-regexp';
 import ListItem from './ListItem'
 
 class VenueList extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      query: '',
-      showingLocations: []
-    }
-  }
-
-  updateQuery(query, locations) {
-    this.setState({ query });
-  }
-
-  filterLocations() {
-    // Filter the locations list using the user input
-    const { locations } = this.props;
-    const { query } = this.state;
-    
-    if (query) {
-      const match = new RegExp(escapeRegExp(query), 'i');
-      return locations.filter((venue) => match.test(venue.name) || match.test(venue.location.address));
-    } else {
-      return locations
-    }
-  }
 
   render() {
     return (
-      <div style={{height: `100%`}}>
-        <input
-          type={"search"}
-          className={"searchInput"}
-          placeholder={"Filter Venues"}
-          value={this.state.query}
-          onChange={(e) => this.updateQuery(e.target.value)}
-          aria-label="Filter Venues"
-        />
+      <div style={{ height: `100%` }}>
         <ul className="places-list">
-          {this.props.venues && this.props.venues.map((venue, idx) => 
-            <ListItem 
-              key={idx} 
-              {...venue} 
+          {this.props.filterVenues.filter && this.props.filterVenues.map((venue, idx) =>
+            <ListItem
+              key={idx}
+              {...venue}
               handleListItemClick={this.props.handleListItemClick}
             />
           )}
